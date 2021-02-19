3fe6d0ea099eb6a5d9bd573b4439f9d7fcb5c784
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2021-02-19 17:46:54 +0600

    legend added

--- Matplotlib/frist_plot.py
+++ Matplotlib/frist_plot.py
@@ -2,10 +2,27 @@
 
 from matplotlib import pyplot as plt
 
-dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000,
          62316, 64928, 67317, 68748, 73572]
 
-plt.plot(dev_x, dev_y)
+# plot for normal developer
+plt.plot(ages_x, dev_y, label='All Devs')
+
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 
+            70000, 71496, 75370, 83640]
+
+# plot for python developer
+plt.plot(ages_x, py_dev_y, label='Python Devs')
+# label the x axis
+plt.xlabel('Ages')
+# label the y axis
+plt.ylabel('Median Salary (USD)')
+# Set title for the plot
+plt.title('Median Salary (USD) by age')
+
+# add lagend for more information
+plt.legend()
+
 plt.show()
